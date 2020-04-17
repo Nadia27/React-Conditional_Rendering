42bc3898e098a26065ee3de7d3721189f805fca0
Nadia Abby <jones.nadia.l@gmail.com>
2020-04-17 11:21:21 -0400

    AND operator
    Use of AND operator in React and conditional rendering

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,22 @@ import React from "react";
 import Login from "./Login";
 
 var isLoggedIn = false;
+const currentTime = new Date().getHours();
+
+console.log(currentTime);
 
 function App() {
   return (
-    <div className="container">{isLoggedIn ? <h1>Hello</h1> : <Login />}</div>
+    <div className="container">
+      {// Ternary Operator
+      isLoggedIn ? <h1>Hello</h1> : <Login />
+
+      // AND Operator
+      // Left side is only evaluated if left is true
+
+      // currentTime < 12 && <h1>Have you started working?</h1>
+      }
+    </div>
   );
 }
 
